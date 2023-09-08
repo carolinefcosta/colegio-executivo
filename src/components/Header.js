3e2fbb419aefc948103d1105d42aacf28016b3a8
Caroline <carolinefernandesc@hotmail.com>
2023-09-07 22:51:49 -0300

    Salvando alterações

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styleHeader from './css/Header.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
@@ -7,17 +7,20 @@ import lupa from '../images/lupa.png';
 
 function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [isPaginaEspecial, setIsPaginaEspecial] = useState(false);
+
+  useEffect(() => {
+    if (window.location.pathname === '/Estrutura') {
+      setIsPaginaEspecial(true);
+    }
+  }, []); 
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
-  const isPaginaEspecial = window.location.pathname === '/Estrutura';
-
-  const textClass = isPaginaEspecial ? styleHeader.textWhite : styleHeader.textBlack;
-
   return (
-    <div className={styleHeader.header}>
+    <div className={`${styleHeader.header} ${isPaginaEspecial ? styleHeader.pageEspecial : ''}`}>
       {/* Botão para o menu de hambúrguer */}
       <button className={styleHeader.menuButton} onClick={toggleMenu}>
         <div className={styleHeader.bar} />
@@ -28,18 +31,18 @@ function Header() {
       {/* Logotipo e texto */}
       <div className={styleHeader.display}>
         <Image className={styleHeader.img} src={logo} alt='logo-executivo' />
-        <Link href='/' className={`${styleHeader.link_col_executivo} ${textClass}`}>Colégio Executivo</Link>
+        <Link href='/' className={`${styleHeader.link_col_executivo}`}>Colégio Executivo</Link>
       </div>
 
       <div className={`${styleHeader.menu} ${menuOpen ? styleHeader.open : ''}`}>
         <div className={styleHeader.links}>
-          <Link href='/' className={`${styleHeader.link} ${textClass}`}>Página Inicial</Link>
-          <Link href='/Niveis' className={`${styleHeader.link} ${textClass}`}>Níveis de ensino</Link>
-          <Link href='/Estrutura' className={`${styleHeader.link} ${textClass}`}>Estrutura</Link>
-          <Link href='/Esportes' className={`${styleHeader.link} ${textClass}`}>Esportes</Link>
-          <Link href='/Diferenciais' className={`${styleHeader.link} ${textClass}`}>Nossos Diferenciais</Link>
-          <Link href='/FaleConosco' className={`${styleHeader.link} ${textClass}`}>Fale Conosco</Link>
-          <Link href='/Parceiros' className={`${styleHeader.link} ${textClass}`}>Parceiros</Link>
+          <Link href='/' className={`${styleHeader.link}`}>Página Inicial</Link>
+          <Link href='/Niveis' className={`${styleHeader.link}`}>Níveis de ensino</Link>
+          <Link href='/Estrutura' className={`${styleHeader.link}`}>Estrutura</Link>
+          <Link href='/Esportes' className={`${styleHeader.link}`}>Esportes</Link>
+          <Link href='/Diferenciais' className={`${styleHeader.link}`}>Nossos Diferenciais</Link>
+          <Link href='/FaleConosco' className={`${styleHeader.link}`}>Fale Conosco</Link>
+          <Link href='/Parceiros' className={`${styleHeader.link}`}>Parceiros</Link>
         </div>
       </div>
       
