fec8e759dccdc279564df0296dec679019e10e63
Caroline <carolinefernandesc@hotmail.com>
2023-09-14 20:37:46 -0300

    Salvando alterações finais

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -1,3 +1,60 @@
+// import Image from 'next/image';
+// import { useState } from 'react';
+// import seta_di from '../images/divisa-direita1.png'
+// import seta_es from '../images/divisa-esquerda1.png'
+// import style from '../components/css/ImageCarousel.module.css'
+
+// const ImageCarousel = ({ images }) => {
+//     const [currentIndex, setCurrentIndex] = useState(0);
+//     const [showButtons, setShowButtons] = useState(false);
+
+//     const prevImage = () => {
+//         setCurrentIndex((currentIndex - 1 + images.length) % images.length);
+//     };
+
+//     const nextImage = () => {
+//         setCurrentIndex((currentIndex + 1) % images.length);
+//     };
+
+//     return (
+//         <div className={style.carousel_container}>
+//             <div className="carousel-slide">
+//                 {images.map((image, index) => (
+//                     <Image
+//                         className={style.img}
+//                         key={index}
+//                         src={image}
+//                         alt={`Imagem ${index + 1}`}
+//                         style={{ display: index === currentIndex ? 'block' : 'none' }}
+//                         onMouseEnter={() => setShowButtons(true)}
+//                         onMouseLeave={() => setShowButtons(false)}
+//                     />
+//                 ))}
+//             </div>
+//             {showButtons && (
+//                 <div className={style.carousel_buttons}>
+//                     <Image
+//                         src={seta_es}
+//                         alt="Anterior"
+//                         className={style.nav_button}
+//                         onClick={prevImage}
+//                     />
+//                     <Image
+//                         src={seta_di}
+//                         alt="Próxima"
+//                         className={style.nav_button}
+//                         onClick={nextImage}
+//                     />
+//                 </div>
+//             )}
+//         </div>
+//     );
+// };
+
+// export default ImageCarousel;
+
+// IMAGENS COM BOTOẼS DE CADA LADO
+
 import Image from 'next/image';
 import { useState } from 'react';
 import seta_di from '../images/divisa-direita1.png'
@@ -17,6 +74,14 @@ const ImageCarousel = ({ images }) => {
 
     return (
         <div className={style.carousel_container}>
+            <div className={style.carousel_buttons}>
+                <Image
+                    src={seta_es}
+                    alt="Anterior"
+                    className={style.nav_button}
+                    onClick={prevImage}
+                />
+            </div>
             <div className="carousel-slide">
                 {images.map((image, index) => (
                     <Image
@@ -28,27 +93,22 @@ const ImageCarousel = ({ images }) => {
                     />
                 ))}
             </div>
-            {/* <button onClick={prevImage}>Anterior</button>
-            <button onClick={nextImage}>Próxima</button> */}
             <div className={style.carousel_buttons}>
-              <Image
-                src={seta_es}
-                alt="Anterior"
-                className={style.nav_button}
-                onClick={prevImage}
-              />
-              <Image
-                src={seta_di}
-                alt="Próxima"
-                className={style.nav_button}
-                onClick={nextImage}
-              />
+                <Image
+                    src={seta_di}
+                    alt="Próxima"
+                    className={style.nav_button}
+                    onClick={nextImage}
+                />
             </div>
         </div>
     );
 };
 
 export default ImageCarousel;
+
+// IMAGECAROUSEL IMAGENS PASSANDO COM INTERVALO TEMPO DE 3 SEGUNDOS
+
 // import React, { useState, useEffect } from "react";
 // import Image from "next/image";
 
